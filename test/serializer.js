aa15da0b265bd676048127b8ae02f093a000b244
digia <me@digia.io>
2015-06-23 21:09:01 -0400

    Add the pluralizeType option.

--- test/serializer.js
+++ test/serializer.js
@@ -30,6 +30,32 @@ describe('Options', function () {
     });
   });
 
+  describe('pluralizeType', function () {
+    it('should allow type to not be pluralized', function (done) {
+      var dataSet = {
+        id: '1',
+        firstName: 'Sandro',
+        lastName: 'Munda',
+      };
+
+      new JsonApiSerializer('user', dataSet, {
+        attributes: ['firstName', 'lastName'],
+        pluralizeType: false
+      }).then(function (json) {
+        expect(json.data.type).equal('user');
+
+        // Confirm it response the same with a truthy setting
+        new JsonApiSerializer('user', dataSet, {
+          attributes: ['firstName', 'lastName'],
+          pluralizeType: true
+        }).then(function (json) {
+          expect(json.data.type).equal('users');
+          done(null, json);
+        });
+      });
+    });
+  });
+
   describe('ref', function () {
     it('should returns the result of the passed function', function (done) {
       var dataSet = [{
