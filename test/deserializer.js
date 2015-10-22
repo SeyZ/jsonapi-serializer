508e8a8e05fc4a20cd115f67287cab6d83fb41d5
sescobb27 <s.escob.b.27@gmail.com>
2015-10-21 21:11:17 -0500

    [add] add support for deserialization

---
+++ test/deserializer.js
@@ -0,0 +1,138 @@
+'use strict';
+/* global describe, it */
+
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+var JsonApiDeSerializer = require('../lib/deserializer');
+
+function Order(attrs) {
+  this.orderType = attrs.orderType;
+  this.productionState = attrs.productionState;
+  this.paymentState = attrs.paymentState;
+  this.shipmentState = attrs.shipmentState;
+  this.createdAt = attrs.createdAt;
+  this.shipDate = attrs.shipDate;
+  this.createdAt = attrs.createdAt;
+  this.shipDate = attrs.shipDate;
+  this.products = attrs.products;
+  this.season = attrs.season;
+}
+
+describe('JSON API DeSerializer', function () {
+  it('should return Object', function () {
+    var dataSet = {
+      data: {
+        attributes: {
+          'order-type': 'subscription',
+          'production-state': 0,
+          'payment-state': 0,
+          'shipment-state': 0,
+          season: 'FA15'
+        },
+        relationships: {},
+        type: 'orders'
+      }
+    };
+    var order = new JsonApiDeSerializer(Order, dataSet);
+    expect(order).to.have.property('orderType', 'subscription');
+    expect(order).to.have.property('productionState', 0);
+    expect(order).to.have.property('paymentState', 0);
+    expect(order).to.have.property('shipmentState', 0);
+    expect(order).to.have.property('season', 'FA15');
+  });
+
+  it('should return Object with many relationships', function () {
+    var dataSet = {
+      data: {
+        attributes: {
+          'order-type': 'subscription',
+          'production-state': 0,
+          'payment-state': 0,
+          'shipment-state': 0,
+          'created-at': null,
+          'ship-date': null,
+          season: 'FA15'
+        },
+        relationships: {
+          plan: {
+            data: {
+              type: 'plans',
+              id: 'uuid-string'
+            }
+          }
+        },
+        type: 'orders'
+      }
+    };
+    var order = new JsonApiDeSerializer(Order, dataSet);
+    console.log(order)
+    expect(order).to.have.property('orderType', 'subscription');
+    expect(order).to.have.property('productionState', 0);
+    expect(order).to.have.property('paymentState', 0);
+    expect(order).to.have.property('shipmentState', 0);
+    expect(order).to.have.property('season', 'FA15');
+    expect(order).to.have.property('plan');
+    expect(order.plan).to.deep.equal({
+      id: 'uuid-string'
+    });
+  });
+
+  it('should return Object with relationships as an Array', function () {
+    var dataSet = {
+      data: {
+        attributes: {
+          'order-type': 'subscription',
+          'production-state': 0,
+          'payment-state': 0,
+          'shipment-state': 0,
+          'created-at': null,
+          'ship-date': null,
+          season: 'FA15'
+        },
+        relationships: {
+          plan: {
+            data: {
+              type: 'plans',
+              id: 'uuid-string'
+            }
+          },
+          products: {
+            data: [{
+              type: 'products',
+              id: 'uuid-string1'
+            }, {
+              type: 'products',
+              id: 'uuid-string2'
+            }, {
+              type: 'products',
+              id: 'uuid-string3'
+            }]
+          }
+        },
+        type: 'orders'
+      }
+    };
+    var order = new JsonApiDeSerializer(Order, dataSet);
+    expect(order).to.have.property('orderType', 'subscription');
+    expect(order).to.have.property('productionState', 0);
+    expect(order).to.have.property('paymentState', 0);
+    expect(order).to.have.property('shipmentState', 0);
+    expect(order).to.have.property('season', 'FA15');
+    expect(order).to.have.property('plan');
+    expect(order.plan).to.deep.equal({
+      id: 'uuid-string'
+    });
+    expect(order).to.have.property('products');
+    expect(order.products.length).to.equal(3);
+    expect(order.products[0]).to.deep.equal({
+      id: 'uuid-string1'
+    });
+    expect(order.products[1]).to.deep.equal({
+      id: 'uuid-string2'
+    });
+    expect(order.products[2]).to.deep.equal({
+      id: 'uuid-string3'
+    });
+  });
+});
